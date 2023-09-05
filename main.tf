657e6a14f93f3d28bb71e438b4f29334a90f5e48
Idan Werber <54882656+GentleGiant-82@users.noreply.github.com>
2023-09-05 08:59:15 +0300

    Update main.tf
    Update subnet

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-  subnet_id = "subnet-02da9d3ff5e5f56bd"
+  subnet_id = "subnet-07ab632f0767b06e7"
 
   tags = {
     Name = "HelloWorld"
