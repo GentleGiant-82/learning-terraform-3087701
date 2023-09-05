9b0737d2df97257a4e9ade745989670af531d560
Idan Werber <54882656+GentleGiant-82@users.noreply.github.com>
2023-09-05 12:17:39 +0300

    Create dev vpc

--- main.tf
+++ main.tf
@@ -15,9 +15,23 @@ data "aws_ami" "app_ami" {
 }
 
 
-#data "aws_vpc" "default" {
-#  default = true
-#}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev_test"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -38,7 +52,8 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = var.default_vpc
+  #vpc_id = var.default_vpc
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -46,43 +61,4 @@ module "blog_sg" {
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
-}
-
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow HTTP and HTTPS in. Allow everything out"
-
-  #vpc_id = data.aws_vpc.default.id
-  vpc_id = var.default_vpc
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
