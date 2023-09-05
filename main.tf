f3879e02f9f4cfabf8313e8bcbf9799991355c15
Idan Werber <54882656+GentleGiant-82@users.noreply.github.com>
2023-09-05 12:32:43 +0300

    Comment out vpc module

--- main.tf
+++ main.tf
@@ -15,22 +15,22 @@ data "aws_ami" "app_ami" {
 }
 
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev_test"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
+#module "vpc" {
+#  source = "terraform-aws-modules/vpc/aws"
+#
+#  name = "dev_test"
+#  cidr = "10.0.0.0/16"
+#
+#  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#
+#  enable_nat_gateway = true
+#
+#  tags = {
+#    Terraform = "true"
+#    Environment = "dev"
+#  }
+#}
 
 
 resource "aws_instance" "blog" {
